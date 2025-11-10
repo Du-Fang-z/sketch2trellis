94a40eb89e505d220b59b1e9f6122acd161921ab
dufan <24066918g@connect.polyu.hk>
2025-11-10 17:29:28 +0800

    new

--- ppp.py
+++ ppp.py
@@ -128,11 +128,18 @@ import random
 import gc
 import os
 
-sam_checkpoint="sam_vit_h_4b8939.pth"
-model_type="vit_h"
+
+
+# 设置设备
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# 加载模型（只执行一次）
+sam_checkpoint = "sam_vit_h_4b8939.pth"
+model_type = "vit_h"
 sam = sam_model_registry[model_type](sam_checkpoint)
 sam.to(device)
+
+# 创建预测器
 predictor = SamPredictor(sam)
 
 # 计算 alpha 区域的中心点和半径
@@ -224,9 +231,5 @@ def run_segmentation(overlay_path, original_path, sam_checkpoint="sam_vit_h_4b89
         f.write(result_image.read())
     print("分割结果已保存为 segmented_result.png")
     
-    del predictor
-    del sam
-    gc.collect()
-    torch.cuda.empty_cache()
 
  
